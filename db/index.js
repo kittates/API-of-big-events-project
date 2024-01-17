dcefe2af2e7d5da8b42b12ae41b33b5cf666eed6
kittates <78903064+kittates@users.noreply.github.com>
2024-01-17 20:07:10 +0800

    Update index.js
    fix

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const db = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'wangzhe2412',
+    password: 'xxxxx',
     database: 'my_db_01'
 })
 
